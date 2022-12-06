af6a139ea3b4255bdb39be7bc8846b6bff66848e
Jhones <jdigno@village88.com>
2022-12-06 22:36:58 +0800

    completed feature

--- assets/js/wall.js
+++ assets/js/wall.js
@@ -1,232 +1,252 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
 
-    let messages = [];
+    let selectedElement;
+    const messagesContainer = document.querySelector("#messages_container");
+    const messageTemplate = document.querySelector("[type='text/html']#message_template");
+    const updateFormTemplate = document.querySelector("[type='text/html']#update_form_template");
+    const messageCount = document.querySelector("#message_count");
+    const noMessage = document.querySelector("#no_message");
+
+    /* Toggling of Modals */
+
+    const modals = document.querySelectorAll(".modal");
+    const modalButtons = document.querySelectorAll(".modal_btn");
+    const modalBodies = document.querySelectorAll(".modal_body");
+    const modalCancels = document.querySelectorAll(".modal_cancel");
+    const modalCloses = document.querySelectorAll(".modal_close");
     
-    let selectedMessage = '';
-    const messagesContainer = document.querySelector('.messages');
-    const modals = document.querySelectorAll('.modal');
-
-    renderMessages();
-
-    //ADD EVENT LISTENER TO EVERY MODAL
+    modalButtons.forEach(modalButton => {
+        modalButton.addEventListener("click", e => {
+            const targetModal = document.querySelector(`#${modalButton.getAttribute("data-target")}`)
+            toggleModal(targetModal);
+        });
+    });
     modals.forEach(modal => {
-        modal.addEventListener('click', e => {
-            closeModal(modal);
-        })
-        modal.querySelector('.modal_body')
-            .addEventListener('click', e => {
-                e.stopPropagation();
+        modal.addEventListener("click", () => {
+            toggleModal(modal);
         });
+    });
+    modalBodies.forEach(modalBody => {
+        modalBody.addEventListener("click", (e) => {
+            e.stopPropagation();
+        });
+    });
+    modalCancels.forEach(modalCancel => {
+        modalCancel.addEventListener("click", () => {
+            toggleModal(modalCancel.closest(".modal"));
+            selectedElement = null;
+        });
+    });
+    modalCloses.forEach(modalClose => {
+        modalClose.addEventListener("click", () => {
+            toggleModal(modalClose.closest(".modal"));
+            selectedElement = null;
+        });
+    });
     
-        modal.querySelector('.modal_close')
-            .addEventListener('click', e => {
-                closeModal(modal)
-        });
-        modal.querySelector('.modal_cancel')
-            .addEventListener('click', e => {
-                closeModal(modal)
-        });
+    /* Creating Message */
+
+    const createMessageForm = document.querySelector("#create_message_form");
+    const btnCreateMessageSubmit = createMessageForm.querySelector("button[type=submit]");
+
+    createMessageForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const newMessage = e.target.elements.content.value;
+        toggleModal(e.target.closest(".modal"));
+        addMessage(newMessage);
+        e.target.reset();
+        btnCreateMessageSubmit.setAttribute("disabled", true);
     })
 
-    //CREATE MODAL
-    const btnCreateMessage = document.querySelector('#btn_create_message');
-    const createModal = document.querySelector('#create_modal');
-    const createMessageForm = document.querySelector('#create_message_form');
+    /* Validating Textarea */
 
-    btnCreateMessage.addEventListener('click', () => {
-        openModal(createModal);
-    })
-
-    // VALIDATION FOR CREATING MESSAGE
-    createMessageForm.querySelector('textarea').oninput = (e) => {
-        if(e.target.value != ''){
-            createMessageForm.querySelector('input[type=submit]').removeAttribute('disabled');
+    createMessageForm.querySelector("textarea").addEventListener("input", (e) => {
+        if(e.target.value.length !== 0){
+            btnCreateMessageSubmit.removeAttribute("disabled");
         }
         else{
-            createMessageForm.querySelector('input[type=submit]').setAttribute('disabled', true);
+            btnCreateMessageSubmit.setAttribute("disabled", true);
+        }
+    });
+
+    /* Delete Message */
+
+    const deleteMessageForm = document.querySelector("#delete_message_form");
+
+    deleteMessageForm.addEventListener("submit", e => {
+        e.preventDefault();
+        toggleModal(e.target.closest(".modal"));
+        if(selectedElement.parentNode?.id == "messages_container"){
+            removeMessage(selectedElement);
+        }
+        else{
+            removeComment(selectedElement);
+        }
+        console.log();
+        selectedElement = null;
+    });
+
+    /* Click Event Bubbling on Message Container */
+
+    messagesContainer.addEventListener("click", (e) => {
+        let targetClass = e.target.className.split(" ")[0];
+        if(e.target?.closest(".message")?.parentNode?.id == "messages_container"){
+            messageControl[targetClass]?.(e.target);
+        }
+        else if(e.target?.closest(".message")?.parentNode?.className == "comments_container"){
+            commentControl[targetClass]?.(e.target);
+        }
+    })
+
+    /* Object Functions - Called depends on the target class name */
+
+    const messageControl = {
+        message_comment: (target) => {
+            let targetMessageComment = target.closest(".message").querySelector(".message_comments");
+            targetMessageComment.classList.toggle("hide");
+            target.classList.toggle("active");
+        },
+        message_delete: (target) => {
+            let targetModal = document.querySelector(`#${target.getAttribute("data-target")}`);
+            targetModal.querySelector("h3").textContent = "Confirm Delete Message";
+            selectedElement = target.closest(".message");
+            toggleModal(targetModal);
+        },
+        message_edit: (target) => {
+            let targetMessage = target.closest(".message");
+            let targetMessageContent = targetMessage.querySelector(".message_content");
+            targetMessageContent.classList.add("hide");
+            addEditForm(targetMessage);
         }
     }
 
-    // CREATE MESSAGE FORM SUBMIT
-    createMessageForm.addEventListener('submit', e => {
-        e.preventDefault();
+    const commentControl = {
 
-        const newMessage = createMessageForm.elements['message'].value;
-        createMessageForm.elements['message'].value = '';
-
-        addMessage(newMessage);
-        renderMessages();
-        closeModal(createModal);
-        createMessageForm.querySelector('input[type=submit]').setAttribute('disabled', true);
-    })
-
-    //DELETE MODAL
-    const deleteModal = document.querySelector('#delete_modal');
-    const deleteMessageForm = document.querySelector('#delete_message_form');
-
-    deleteMessageForm.addEventListener('submit', e => {
-        e.preventDefault();
-        deleteMessage(selectedMessage);
-        renderMessages();
-        closeModal(deleteModal);
-    })
-
-    //EVENT BUBBLIGN FROM MESSAGES CONTAINER
-    messagesContainer.addEventListener('click', e => {
-        
-        if(e.target.parentNode.className == 'message_delete'){
-            openModal(deleteModal);
-            selectedMessage = e.target.parentNode.closest('.message_delete').getAttribute('data-id');
-        } 
-        else if(e.target.parentNode.className == 'message_edit'){
-            let targetElement = e.target.parentNode;
-            let targetMessage = targetElement.parentNode.parentNode;
-            selectedMessage = targetElement.closest('.message_edit').getAttribute('data-id');
-            let clonedMessage = targetMessage.innerHTML;
-
-            targetMessage.innerHTML = `
-            <form action="#" class="update_form">
-                <textarea 
-                    name="content" 
-                    id="content" 
-                    cols="30" 
-                    rows="3"
-                    placeholder="Type your comment here"
-                >${targetMessage.childNodes[1].textContent}</textarea>
-                <div>
-                    <a href="#" class="cancel_edit">Cancel</a>
-                    <input type="submit" value="Update Message" class="btn btn-primary btn-small">
-                </div>
-            </form>
-            `;
-
-            //ADD EVENT LISTENER TO UPDATE FORM
-            targetMessage.childNodes[1].addEventListener('submit', e => {
-                e.preventDefault();
-                let formData = getFormData(targetMessage.childNodes[1]);
-                updateMessage(selectedMessage, formData.content);
-                renderMessages();
-            });
-
-
-            let cancelEditBtn = targetMessage.childNodes[1].closest('div').querySelector('.cancel_edit');
-
-            cancelEditBtn.addEventListener('click', e => {
-                e.preventDefault();
-                targetMessage.innerHTML = clonedMessage;
-            })
-        }
-    })
-
-    //RENDERS
-    function renderMessages(){
-        document.querySelector('.message_count').innerHTML = messages.length;
-
-        messagesContainer.innerHTML = '';
-
-        if(messages.length < 1){
-            messagesContainer.innerHTML = `
-            <div class="no_message">
-                <img src="../assets/images/no-message.svg" alt="no_message">
-                <p>No Posted Message Yet</p>
-            </div> 
-            `;
-        }
-        else {
-            for(let i = messages.length-1; i >= 0; i--){
-                messagesContainer.innerHTML += `
-                    <div class="message">
-                        <div class="message_content">
-                            <p>${messages[i].content}</p>
-                            <div class="message_actions">
-                                <a href="#" class="message_comment">
-                                    <img src="../assets/images/message.svg" alt="message-icon">
-                                    <p>
-                                        <span>
-                                        ${
-                                            messages[i].comments
-                                                ? messages[i].comments.length
-                                                : 0
-                                        }
-                                        </span>
-                                        Comment/s
-                                    </p>
-                                </a>
-                                <a href="#" class="message_edit" data-id="${messages[i].id}">
-                                    <img src="../assets/images/pencil-write.svg" alt="edit-icon">
-                                    <p>Edit</p>
-                                </a>
-                                <a href="#" class="message_delete" data-id="${messages[i].id}">
-                                    <img src="../assets/images/delete.svg" alt="delete-icon">
-                                    <p>Delete</p>
-                                </a>
-                                <a href="#" class="message_user" data-id="${messages[i].id}">
-                                    <img src="../assets/images/user-placeholder.svg" alt="user-icon">
-                                    <p><span>You</span> - Few seconds ago</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                `;
-            }
+        message_delete: (target) => {
+            let targetModal = document.querySelector(`#${target.getAttribute("data-target")}`);
+            targetModal.querySelector("h3").textContent = "Confirm Delete Comment";
+            selectedElement = target.closest(".message");
+            toggleModal(targetModal);
+        },
+        message_edit: (target) => {
+            let targetMessage = target.closest(".message");
+            let targetMessageContent = targetMessage.querySelector(".message_content");
+            targetMessageContent.classList.add("hide");
+            addEditForm(targetMessage, "Comment Update");
         }
     }
-
-    //STATIC ARRAYS OPERATION
+    /* Render Functions */
 
     function addMessage(newMessage){
-        messages.push({
-            id: generateId(),
-            content: newMessage,
-            comments: [],
-        })
-    }
+        let newMessageTemplate = messageTemplate.cloneNode(true).innerHTML;
+        messagesContainer.insertAdjacentHTML("afterbegin", newMessageTemplate);
 
-    function deleteMessage(id){
-        messages = messages.filter(message => message.id != id);
-        selectedMessage = '';
-    }
-
-    function updateMessage(id, newMessage){
-        messages = messages.map(message => {
-            if(id == message.id){
-                return { ...message, content: newMessage };
-            }
-
-            return message;
-        })
-    }
-
-    function getComments(id){
-        let message = messages.find(message => message.id == id);
-        return message.comments || null;
-    }
-
-    function addComment(id, data){
-        let newComment = {
-            id: generateId(),
-            content: data
+        let newMessageContainer = messagesContainer.querySelector(".message");
+        newMessageContainer.querySelector(".message_content>p").textContent = newMessage;
+       
+        messageCount.textContent = +messageCount.textContent + 1;
+        if(+messageCount.textContent >= 1){
+            noMessage.classList.add("hide");
         }
 
-        messages = messages.map(message => {
-            if(id == message.id){
-                return { ...message, comments: [...message.comments, newComment ] }
+        let addCommentForm = newMessageContainer.querySelector(".message form");
+        let btnCommentSubmit = addCommentForm.querySelector("button[type=submit]");
+
+        addCommentForm.querySelector("textarea").addEventListener("input", (e) => {
+            if(e.target.value.length !== 0){
+                btnCommentSubmit.removeAttribute("disabled");
             }
-            return message;
+            else{
+                btnCommentSubmit.setAttribute("disabled", true);
+            }
+        });
+
+        addCommentForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let newComment = e.target.elements.content.value;
+            addComment(newMessageContainer, newComment);
+            e.target.reset();
+            btnCommentSubmit.setAttribute("disabled", true);
+        });
+    }
+
+    function addComment(messageContainer, newComment){
+
+        let commentCount = messageContainer.querySelector(".message_actions .message_comment span");
+        let newCommentTemplate = messageTemplate.cloneNode(true).innerHTML;
+        let newCommentContainer = messageContainer.querySelector(".comments_container");
+
+        newCommentContainer.insertAdjacentHTML("afterbegin", newCommentTemplate);
+        newCommentContainer.querySelector(".message_content>p").textContent = newComment;
+        newCommentContainer.querySelector(".message_comment").remove();
+        newCommentContainer.querySelector(".message_comments").remove();
+
+        console.log(messageContainer);
+
+        commentCount.textContent = +commentCount.textContent + 1;
+    }
+
+    function removeMessage(messageElement){
+        messageElement.remove();
+        messageCount.textContent = +messageCount.textContent - 1;
+        if(+messageCount.textContent <= 0){
+            noMessage.classList.remove("hide");
+        }
+    }
+
+    function removeComment(commentElement){
+        let commentCount = commentElement.parentNode.closest(".message").querySelector(".message_actions span");
+        commentCount.textContent = +commentCount.textContent - 1;
+        commentElement.remove();
+    }
+
+    function addEditForm(containerElement, submitText = "Message Update"){
+        let messageContentElement = containerElement.querySelector(".message_content");
+        let formElement = updateFormTemplate.cloneNode(true).innerHTML;
+
+        containerElement.insertAdjacentHTML("afterbegin", formElement);
+        containerElement.querySelector("form textarea").value = messageContentElement.querySelector("p").textContent;
+        containerElement.querySelector("form button").textContent = submitText;
+
+        let newFormElement = containerElement.querySelector("form");
+        let btnFormSubmit = newFormElement.querySelector("button[type=submit]");
+
+        newFormElement.querySelector("textarea").addEventListener("input", (e) => {
+            if(e.target.value.length !== 0){
+                btnFormSubmit.removeAttribute("disabled");
+            }
+            else{
+                btnFormSubmit.setAttribute("disabled", true);
+            }
+        })
+
+        newFormElement.querySelector(".cancel_edit").addEventListener("click", () => {
+            removeEditForm(containerElement, messageContentElement);
+        });
+
+        newFormElement.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let newContent = e.target.elements.content.value;
+            editForm(containerElement, messageContentElement, newContent);
         })
     }
 
-    //HELPER FUNCTIONS
+    /* Edit Form Event Listeners */
 
-    function openModal(modalElement){
-        modalElement.classList.add('show');
+    function removeEditForm(containerElement, messageContentElement){
+        messageContentElement.classList.remove("hide");
+        containerElement.querySelector("form").remove();
     }
 
-    function closeModal(modalElement){
-        modalElement.classList.remove('show');
+    function editForm(containerElement, messageContentElement, newContent){
+        messageContentElement.querySelector("p").textContent = newContent;
+        messageContentElement.classList.remove("hide");
+        containerElement.querySelector("form").remove();
     }
 
-    function generateId(){
-        return Date.now().toString(36) + Math.random().toString(36);
+    /* Helper Functions */
+
+    function toggleModal(modalElement){
+        modalElement.classList.toggle("show");
     }
 });
