e7b26149258463caf999710266593d411ff1f2c9
Jhones <jdigno@village88.com>
2022-12-15 12:40:12 +0800

    Added comments and default data-message/comment-id in html

--- assets/js/wall.js
+++ assets/js/wall.js
@@ -18,6 +18,7 @@ window.addEventListener("load", () => {
 
 });
 
+/* Submit listener for create message form that will add new message*/
 function submitAddMessage(event){
     event.preventDefault();
     let create_message_form = event.target; 
@@ -29,6 +30,8 @@ function submitAddMessage(event){
     new_message.removeAttribute("id");
     new_message.setAttribute("data-message-id", generateId());
     new_message.querySelector(".message_content>p").textContent = new_content;
+
+    /* Adding event Listeners for new message */
     new_message.querySelector(".update_form").addEventListener("submit", submitUpdateContent);
     new_message.querySelector(".update_form .cancel_edit").addEventListener("click", clickCancelEdit);
     new_message.querySelector(".update_form textarea").addEventListener("input", inputValidate);
@@ -37,14 +40,16 @@ function submitAddMessage(event){
     new_message.querySelector(".message_actions .message_comment").addEventListener("click", clickCommentBtn);
     new_message.querySelector(".message_actions .message_edit").addEventListener("click", clickEditBtn);
     new_message.querySelector(".message_actions .message_delete").addEventListener("click", clickDeleteMessageBtn);
+    
+    /* Prepend the new message and reset form and modal */
     document.querySelector("#messages_container").prepend(new_message);
-
     updateMessagesCount();
     submit_btn.setAttribute("disabled", true);
     create_message_form.reset();
     toggleModal(create_message_form.closest(".modal"));
 }
 
+/* Submit event listener for delete message form */
 function submitDeleteMessage(event){
     event.preventDefault();
     let delete_message_form = event.target;
@@ -54,6 +59,7 @@ function submitDeleteMessage(event){
     toggleModal(delete_message_form.closest(".modal"));
 }
 
+/* Submit event listener for delete comment form */
 function submitDeleteComment(event){
     event.preventDefault();
     let delete_comment_form = event.target;
@@ -65,6 +71,7 @@ function submitDeleteComment(event){
     toggleModal(delete_comment_form.closest(".modal"));
 }
 
+/* Will close the modal */
 function clickCloseModal(event){
     let target = event.target;
     if(
@@ -82,6 +89,7 @@ function clickCloseModal(event){
     }
 }
 
+/* Will toggle comment container in message */
 function clickCommentBtn(event){
     let comment_btn = event.target;
     let comment_container = comment_btn.closest(".message").querySelector(".message_comments");
@@ -92,6 +100,7 @@ function clickCommentBtn(event){
     comment_btn.classList.toggle("active");
 }
 
+/* Will open delete message form modal and set data-message-id in it's hidden input */
 function clickDeleteMessageBtn(event){
     event.preventDefault();
     let delete_btn = event.target;
@@ -101,6 +110,7 @@ function clickDeleteMessageBtn(event){
     toggleModal(target_modal);
 }
 
+/* Will open delete comment form modal and set data-comment-id in it's hidden input */
 function clickDeleteCommentBtn(event){
     event.preventDefault();
     let delete_btn = event.target;
@@ -110,8 +120,8 @@ function clickDeleteCommentBtn(event){
     toggleModal(target_modal);
 }
     
+/* Will show update form and hide the content */
 function clickEditBtn(event){
-    event.preventDefault();
     let edit_btn = event.target;
     let message_content = edit_btn.closest(".message_content");
     let update_form = edit_btn.closest(".message").querySelector(".update_form");
@@ -124,12 +134,14 @@ function clickEditBtn(event){
     textarea.focus();
 }
 
+/* Will hide update form and show the content */
 function clickCancelEdit(event){
     let edit_button = event.target;
     edit_button.closest(".update_form").classList.add("hide");
     edit_button.closest(".message").querySelector(".message_content").classList.remove("hide");
 }
 
+/* Submit listener for create comment form that will add comment to the message*/
 function submitAddComment(event){
     event.preventDefault();
     let add_comment_form = event.target;
@@ -138,6 +150,7 @@ function submitAddComment(event){
     let new_content = add_comment_form.elements.content.value;
     let submit_btn = add_comment_form.querySelector("button[type='submit']");
 
+    /* Adding event listeners for new comment */
     new_comment.classList.remove("hide");
     new_comment.removeAttribute("id");
     new_comment.setAttribute("data-comment-id", generateId());
@@ -149,12 +162,14 @@ function submitAddComment(event){
     new_comment.querySelector(".update_form").addEventListener("submit", submitUpdateContent);
     comment_container.prepend(new_comment);
 
+    /* Prepend the new comment and reset form */
     updateCommentsCount(add_comment_form.closest(".message"));
     submit_btn.setAttribute("disabled", true);
     add_comment_form.reset();
     add_comment_form.querySelector("textarea").focus();
 }
 
+/* Submit event listener for update form, this will update the content */
 function submitUpdateContent(event){
     event.preventDefault();
     let update_form = event.target;
@@ -166,23 +181,25 @@ function submitUpdateContent(event){
     message_content.classList.remove("hide");
 }
 
-/* Helper Functions */
-
+/* Will open the create message form modal */
 function createMessageClick(){
     toggleModal(document.querySelector(`#create_modal`));
     create_message_form.querySelector("textarea").focus();
 }
 
+/* Will toggle modal and expecting modal element as argument */
 function toggleModal(modal_element){
     modal_element.classList.toggle("show");
 }
 
+/* Will update the comments count based on length of child elements of current comment container */
 function updateCommentsCount(message_container){
     let count_element = message_container.querySelector(".message_actions .message_comment span");
     let comment_container = message_container.querySelector(".comments_container");
     count_element.textContent = comment_container.children.length;
 }
 
+/* Will update the message count based on length of child elements of messages container */
 function updateMessagesCount(){
     let new_count = document.querySelector("#messages_container").children.length;
     document.querySelector("#message_count").textContent = new_count;
@@ -194,18 +211,21 @@ function updateMessagesCount(){
     }
 }
 
+/* Will validate the input field by disabling the submit button */
 function inputValidate(event){
     let input = event.target;
     let btn_submit = input.closest("form").querySelector("button[type='submit']");
-
-    if(input.value.length !== 0){
-        btn_submit.removeAttribute("disabled");
+    
+    /* Will disable submit button if the input length is less than 1 */
+    if(input.value.length < 1){
+        btn_submit.setAttribute("disabled", true);
     }
     else{
-        btn_submit.setAttribute("disabled", true);
+        btn_submit.removeAttribute("disabled");
     }
 }
 
+/* Will generate random number for id purposes */
 function generateId(){
     return Math.ceil(Date.now() + Math.random());
 }
